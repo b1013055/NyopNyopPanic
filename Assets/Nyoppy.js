e1a89cd0770bdb716932fb772a6d5c00ccbe77ff
矢野颯太 <Kuro@Krkg01.local>
2015-10-11 16:38:00 +0900

    first commit

---
+++ Assets/Nyoppy.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+var myCube:GameObject;
+function Start () 
+{
+}
+
+function Update () 
+{
+}
+
+function CreateNyoppy()
+{
+
+	Instantiate (myCube, Vector3(0, 30, 0), Quaternion.identity);
+}
+
